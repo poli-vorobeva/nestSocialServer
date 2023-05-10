43f3817577d092cc2c3f93e82f80a243eab9b8f6
poli8512 <poli8512@gmail.com>
2023-05-10 23:18:52 +0300

    fix

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -10,7 +10,7 @@ export class FileService {
 		try {
 			const fileExtension = file.originalname.split('.').pop()
 			const fileName = uuid.v4() + '.'+fileExtension
-			const filePath = path.resolve(__dirname, '..', 'static', 'images')
+			const filePath = path.resolve(__dirname, 'static', 'images')
 			if (!fs.existsSync(filePath)) {
 				fs.mkdirSync(filePath, {recursive: true})
 			}
